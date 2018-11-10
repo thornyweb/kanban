fe82d3da939cee5745a4ccd2ba592c12b8c41e90
Michael Thorn <michael@openobjects.com>
2018-11-10 00:33:49 +0000

    Add link colors

--- src/Components/HeaderNav/index.jsx
+++ src/Components/HeaderNav/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import './styles.scss';
 
 export default class HeaderNav extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <nav className='navbar navbar-expand-lg bg-secondary'>
+        <nav className='navbar navbar-expand-md bg-secondary'>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span class='navbar-toggler-icon' />
+            Menu
+          </button>
           <div className='collapse navbar-collapse'>
             <ul className='navbar-nav mr-auto'>
               <li className='nav-item'>
